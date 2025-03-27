c18f4cad07fbc5e00704dbef0e4686d27d044dc9
RPChacon <150461481+RPChacon@users.noreply.github.com>
2025-03-27 10:14:46 -0300

    Create ex07.py

---
+++ atividade01/ex07.py
@@ -0,0 +1,3 @@
+n = int(input("Digite um número ímpar: "))
+diferenca = ((n-1) * (n-1)) - ((n+1) * (n+1))
+print (f"A diferença entre o quadrado do número anterior e do próximo número é {diferenca}.")
